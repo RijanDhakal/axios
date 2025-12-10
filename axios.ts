11bd0cb494759a85e717725c6732a828823d2cbb
Rijan Dhakal <rijandhakal0619@gmail.com>
2025-12-11 01:36:32 +0545

    add post method and improved error handling and timeout management

--- axios.ts
+++ axios.ts
@@ -1,7 +1,13 @@
 interface config {
-  headers?: Record<string, string>;
+  headers: Record<string, string>;
   baseUrl?: string;
   timeout?: number;
+}
+
+interface FetchData {
+  fetchUrl: string;
+  method?: "GET" | "POST" | "PATCH" | "DELETE" | "PUT";
+  data?: string;
 }
 
 class Axios {
@@ -9,15 +15,96 @@ class Axios {
     headers: {
       "Content-Type": "application/json",
     },
+    timeout: 1000,
   };
   constructor(config: config) {
     this.config = config;
   }
+
+  private async handleTimeOut(fetchData: FetchData) {
+    if (!this.config.baseUrl) return;
+    const controller = new AbortController();
+    const timeOutId = setTimeout(() => {
+      controller.abort();
+    }, this.config.timeout);
+
+    if (fetchData.method === "GET") {
+      let fetchApi;
+      try {
+        fetchApi = await fetch(fetchData.fetchUrl, {
+          signal: controller.signal,
+        });
+      } catch (error: any) {
+        if (error.name === "AbortError") {
+          throw new Error("Request time out");
+        } else {
+          throw error;
+        }
+      }
+      clearTimeout(timeOutId);
+      const JsonResponse = await fetchApi?.json();
+      if (!fetchApi) {
+        throw new Error("no fetch data found");
+      }
+      return JsonResponse;
+    }
+    if (fetchData.method === "POST") {
+      let fetchApi;
+      try {
+        fetchApi = await fetch(fetchData.fetchUrl, {
+          signal: controller.signal,
+          method: fetchData.method,
+          headers: this.config.headers,
+          body: JSON.stringify(fetchData.data),
+        });
+      } catch (error: any) {
+        if (error.name === "AbortError") {
+          throw new Error("Request time out");
+        }
+        throw error;
+      }
+      clearTimeout(timeOutId);
+      const JsonResponse = await fetchApi?.json();
+      if (!fetchApi) {
+        throw new Error("no fetch data found");
+      }
+      return JsonResponse;
+    }
+  }
+
   async get(url?: string) {
     if (!this.config.baseUrl) return;
-    const fetchResponse = await fetch(this.config.baseUrl + url);
-    const resJson = await fetchResponse.json();
-    return resJson;
+    const fetchUrl = this.config.baseUrl + url;
+    const fetchResponse = await this.handleTimeOut({
+      fetchUrl: fetchUrl,
+      method: "GET",
+    });
+    if (!fetchResponse) {
+      throw new Error("No response received");
+    }
+    if (!(fetchResponse.statusCode === 200)) {
+      throw new Error(fetchResponse);
+    }
+    return fetchResponse;
+  }
+
+  async post({ url, payload }: { url: string; payload: any }) {
+    if (!url || !this.config.baseUrl) {
+      throw new Error("No url received !");
+    }
+    const fetchUrl = this.config.baseUrl + url;
+    const fetchResponse = await this.handleTimeOut({
+      fetchUrl: fetchUrl,
+      method: "POST",
+      data: payload,
+    });
+    if (!fetchResponse) {
+      throw new Error("No response received");
+    }
+    if (!(fetchResponse.statusCode === 201)) {
+      throw new Error(fetchResponse.message);
+    }
+    return fetchResponse;
   }
 }
 
--- demo.ts
+++ demo.ts
@@ -1,11 +1,29 @@
 import axios from "./axios";
 
 const api = axios.create({
-  baseUrl: "https://randomuser.me",
+  baseUrl: "http://localhost:3000",
+  timeout: 40,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 const asyncFunction = async () => {
-  const res = await api.get("/api");
-  console.log(res);
+  const res = await api.get("/students/get");
+  //   console.log("res :", res);
+  // const res = await api.post({
+  //   url: "/students/create",
+  //   payload: {
+  //     studentId: 10,
+  //     name: "test",
+  //     address: "sunwal",
+  //     contact: "6767327832",
+  //     grade: 12,
+  //     section: 106,
+  //     stream: "SCIENCE",
+  //     password: "123456",
+  //   },
+  // });
+  console.log("res :", res);
 };
 asyncFunction();
